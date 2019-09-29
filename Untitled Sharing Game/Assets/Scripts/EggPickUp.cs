6f3145603357d00078a7b5694a13af9d38a12322
Mturtle7 <sabertoothkim@gmail.com>
2019-09-28 22:34:51 -0700

    Player picks up egg to top of head
    When an Egg detects a collision with a Player, it moves itself to position (1, 1, 1) relative to the Player.

---
+++ Untitled Sharing Game/Assets/Scripts/EggPickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggPickUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //called when this Egg collides with a Player
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("Egg was hit by Player, otherCollider = " + col.otherCollider + "incoming = " + col.gameObject);
+
+            //instantly move egg to attach to bottom left corner of Player
+            col.otherCollider.gameObject.transform.parent = col.gameObject.transform;
+            Debug.Log("Egg's coords are " + col.otherCollider.gameObject.transform.localPosition + "relative to " + col.gameObject);
+            col.otherCollider.gameObject.transform.localPosition = new Vector3(1, 1, 1);
+            Debug.Log("Egg's coords are " + col.otherCollider.gameObject.transform.localPosition + "relative to " + col.gameObject);
+        }
+
+    }
+}
