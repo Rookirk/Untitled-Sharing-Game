6156850751ff9200b0620a2ec242552d9fa0824b
Rookirk <hwu24110@gmail.com>
2019-09-29 14:09:53 -0700

    Added button hovering for play button

---
+++ Untitled Sharing Game/Assets/Scripts/playButtonHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playButtonHover : MonoBehaviour
+{
+	public Sprite unHover;
+	public Sprite yesHover;
+	private Image imageComp;
+
+	void Start(){
+		imageComp = GetComponent<Image>();
+	}
+
+    public void changeOnHover(){
+    	imageComp.sprite = yesHover;
+    }
+
+    public void notHovering(){
+    	imageComp.sprite = unHover;
+    }
+}
