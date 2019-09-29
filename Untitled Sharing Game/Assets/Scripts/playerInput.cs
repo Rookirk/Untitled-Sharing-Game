1e9c569207d6a8324e556840a9240a92ba67604a
BowTies28 <yoshiniko1223@gmail.com>
2019-09-28 20:44:59 -0700

    Made Move Lefty and move Righty
    -Added playerInput script
    --This allows the player to move left and right
    --*** however there are two players and they move at the same time because i just copied the object and did that so i'll fix it up soon:tm:

---
+++ Untitled Sharing Game/Assets/Scripts/playerInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Apparently I'm supposed to put this here to make sure
+//that the script runs before everything else
+//it helps it not make the game feel laggy
+[DefaultExecutionOrder(-100)]
+public class playerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+
+    private Vector2 moveVelocity;
+    private Rigidbody2D rb;
+    
+    
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
